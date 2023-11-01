6797366f7236a73bc39860a9969fd3a4e9f2bd02
Raul Madero <r.madero.ramirez@gmail.com>
2023-11-01 11:56:49 -0600

    Agregando funciones random

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const card = document.querySelector("#carta");
+  const topSuit = document.querySelector("#topSuit");
+  const value = document.querySelector(".value");
+  const bottomSuit = document.querySelector("#bottomSuit");
+  let suits = ["hearts", "spades", "clubs", "diamonds"];
+  let cardValue = Math.floor(Math.random() * 12);
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  function insertSuit() {
+    if (suits[randomSuit] === "hearts") {
+      card.classList.add("hearts");
+      topSuit.innerHTML = `<div class="suit m-0 p-0"><p class="hearts p-0">♥</p></div>`;
+      value.innerHTML = `<p class="number m-0 p-0">${cardValue}</p>`;
+      bottomSuit.innerHTML = `<div class="suit mb-2 p-0"><p class="hearts p-0">♥</p></div>`;
+    } else if (suits[randomSuit] === "spades") {
+      card.classList.add("spades");
+      topSuit.innerHTML = `<div class="suit m-0 p-0"><p class="spades mt-n3 p-0">♠</p></div>`;
+      value.innerHTML = `<p class="number m-0 p-0">${cardValue}</p>`;
+      bottomSuit.innerHTML = `<div class="suit mb-2 p-0"><p class="spades mt-n3 p-0">♠</p></div>`;
+    } else if (suits[randomSuit] === "clubs") {
+      card.classList.add("clubs");
+      topSuit.innerHTML = `<div class="suit m-0 p-0"><p class="clubs mt-n3 p-0">♣</p></div>`;
+      value.innerHTML = `<p class="number m-0 p-0">${cardValue}</p>`;
+      bottomSuit.innerHTML = `<div class="suit mb-2 p-0"><p class="clubs mt-n3 p-0">♣</p></div>`;
+    } else if (suits[randomSuit] === "diamonds") {
+      card.classList.add("diamonds");
+      topSuit.innerHTML = `<div class="suit m-0 p-0"><p class="diamonds mt-n3 p-0">♦</p></div>`;
+      value.innerHTML = `<p class="number m-0 p-0">${cardValue}</p>`;
+      bottomSuit.innerHTML = `<div class="suit mb-2 p-0"><p class="diamonds mt-n3 p-0">♦</p></div>`;
+    }
+  }
+  insertSuit();
+  console.log(randomSuit);
+  console.log(cardValue);
 };
